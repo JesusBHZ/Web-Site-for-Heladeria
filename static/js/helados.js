0843284f8fade0a3fabe0224607d79280f9b79e8
JesusBHZ <1721110061@utectulancingo.edu.mx>
2023-02-03 20:42:47 -0600

    more code

--- static/js/helados.js
+++ static/js/helados.js
@@ -1,1 +1,19 @@
+function showTipo(){
+    let tipo = document.getElementById('tipo_helado');
+    let valor = document.getElementById('tipo');
+    if(tipo.checked==true){
+        valor.innerHTML = "Especial"
+    }else{
+        valor.innerHTML = "Clasico"
+    }
+}
 
+function showTama(){
+    let tipo = document.getElementById('tipo_tamano');
+    let valor = document.getElementById('tipotama');
+    if(tipo.checked==true){
+        valor.innerHTML = "Doble"
+    }else{
+        valor.innerHTML = "Sencillo"
+    }
+}
